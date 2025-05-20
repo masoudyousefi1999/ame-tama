48365f3584e1dcca686e7d7340b1686e7fb91f3b
masoudyousefi <masoudyousefi1999@gmail.com>
2025-05-20 12:18:54 +0430

    fix(hero-section)

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -16,7 +16,7 @@ export default function HeroSection() {
   };
 
   return (
-    <section className="relative min-h-screen h-[100dvh] flex items-center justify-center overflow-hidden">
+    <section className="relative min-h-[100svh] h-[100svh] flex items-center justify-center overflow-hidden">
       {/* Background Image */}
       <div className="absolute inset-0 z-0">
         <Image
@@ -82,7 +82,7 @@ export default function HeroSection() {
       </div>
 
       {/* Bottom Gradient Fade */}
-      <div className="absolute bottom-0 left-0 right-0 h-24 bg-gradient-to-t from-gray-900/80 to-transparent z-20" />
+      <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-black/60 via-black/30 to-transparent z-20 pointer-events-none" />
 
       {/* Scroll Button */}
       <motion.button
@@ -91,7 +91,7 @@ export default function HeroSection() {
         initial={{ opacity: 0, y: 10 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, delay: 0.6 }}
-        className="absolute bottom-6 left-1/2 transform -translate-x-1/2 z-30 flex items-center justify-center"
+        className="absolute bottom-4 sm:bottom-6 left-1/2 transform -translate-x-1/2 z-30 flex items-center justify-center"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
